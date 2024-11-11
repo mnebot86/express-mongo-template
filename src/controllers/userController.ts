d48eddf18f0259e9cf5218e2c4135a3fa00135e7
mnebot86 <miguelnbt@gmail.com>
2024-11-11 18:05:30 -0500

    husky is working now

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, RequestHandler, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 import * as userService from '../services/userService';
 
@@ -8,13 +9,16 @@ export const registerUser: RequestHandler = async (req, res) => {
 
 		const existingUser = await userService.findUserByEmail(email);
 		if (existingUser) {
-			res.status(400).json({ message: 'User with this email already exists' });
+			res.status(StatusCodes.BAD_REQUEST).json({
+				message: 'User with this email already exists',
+			});
 
 			return;
 		}
 
 		const user = await userService.createUser(email, password);
 		const token = userService.generateToken(user);
+
 		res.status(201).json({ message: 'User registered successfully', user, token });
 	} catch (error) {
 		res.status(500).json({ message: 'Internal server error' });
